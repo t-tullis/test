135fb6825334821b289ce612eb269196d62f23ff
t-tullis <t.tullis34@gmail.com>
2021-01-27 21:18:36 -0800

    added flag

--- DB/dbConnection.js
+++ DB/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true  })
+    await mongoose.connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true , useFindAndModify: false })
     console.log('Mongo DB Connected')
 }
 
